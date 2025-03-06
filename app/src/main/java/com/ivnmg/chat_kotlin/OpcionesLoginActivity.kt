ec302c5fa49857a057273abd21b792156a25b4f8
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-06 14:00:48 -0600

    Interfaz Login

--- app/src/main/java/com/ivnmg/chat_kotlin/OpcionesLoginActivity.kt
+++ app/src/main/java/com/ivnmg/chat_kotlin/OpcionesLoginActivity.kt
@@ -1,20 +1,30 @@
 package com.ivnmg.chat_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ivnmg.chat_kotlin.databinding.ActivityMainBinding
+import com.ivnmg.chat_kotlin.databinding.ActivityOpcionesLoginBinding
 
 class OpcionesLoginActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityOpcionesLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_opciones_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.opcionEmail.setOnClickListener{
+            startActivity(Intent(applicationContext, LoginEmailActivity::class.java))
         }
+
     }
 }
